29de04b1eb7f7b45fdb20ce82650d453020089e3
Robert W Boyer <rwboyer@mac.com>
2016-08-13 00:10:45 -0400

    start countdown clock and newsletter

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 /* jshint ignore:start */
 // @codekit-prepend "../bower_components/jquery/dist/jquery.js";
+// @codekit-prepend "../bower_components/jquery.countdown/dist/jquery.countdown.js";
 /* jshint ignore:end */
 // @codekit-append "src/height.js";
 // @codekit-append "src/menu.js";

34b71a875b18b7f6a27c8bcdda7b8b78258bdfa9
Robert W Boyer <rwboyer@mac.com>
2016-08-15 14:19:58 -0400

    yet again

--- js/src/mailchimp.js
+++ js/src/mailchimp.js
@@ -1,16 +1,15 @@
 
-  function handle_mailchimp(){
-    jQuery.get('https://heroku-mailgun.herokuapp.com/mailchimp-api/list?first=bob&last=hamilton&email=bob.hamilton@offtheeatenpath.bike');
-    jQuery.ajax({
-      url: 'https://heroku-mailgun.herokuapp.com/mailchimp-api/list?first=bob&last=hamilton&email=bob.hamilton@offtheeatenpath.bike',
-      dataType: "jsonp",
-      success: function (data) {
-        console.log(data)
-        alert(data);
-      }
-    });
-    console.log('called function');
-    event.preventDefault();
-    return false;
-  }
+function handle_mailchimp(){
+  jQuery.ajax({
+    url: 'https://heroku-mailgun.herokuapp.com/mailchimp-api/list?first=bob&last=hamilton&email=bob.hamilton@offtheeatenpath.bike',
+    dataType: "jsonp",
+    success: function (data) {
+      console.log(data)
+      alert(data);
+    }
+  });
+  console.log('called function');
+  event.preventDefault();
+  return false;
+}
   

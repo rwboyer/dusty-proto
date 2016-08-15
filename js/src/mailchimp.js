f9ae70f2b5ab2a0b6e9ff933558967d8768610e1
Robert W Boyer <rwboyer@mac.com>
2016-08-15 14:04:02 -0400

    new ssl test

--- js/src/mailchimp.js
+++ js/src/mailchimp.js
@@ -1,6 +1,6 @@
 
   function handle_mailchimp(){
-    jQuery.get('http:chimp.offtheeatenpath.bike/mailchimp-api/list?first=bob&last=hamilton&email=bob.hamilton@offtheeatenpath.bike');
+    jQuery.get('https://heroku-mailgun.herokuapp.com/mailchimp-api/list?first=bob&last=hamilton&email=bob.hamilton@offtheeatenpath.bike');
     console.log('called function');
     event.preventDefault();
     return false;

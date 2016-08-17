278ee6a0119844e00c7b7d731bf8450a2ca7424c
Robert W Boyer <rwboyer@mac.com>
2016-08-17 13:24:13 -0400

    sign-up dialog.

--- js/src/mailchimp.js
+++ js/src/mailchimp.js
@@ -15,7 +15,7 @@ function handle_mailchimp(){
   });
   console.log('sign-up');
   event.preventDefault();
-  alert('Thank You');
+  $(".modal-state#modal-3").prop("checked", true).change();
   return false;
 }
   
